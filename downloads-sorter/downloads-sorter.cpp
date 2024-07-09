ee03670fa8957a3f39a4d8dc8c55bd60680502a7
madbearing <t79601712352@gmail.com>
2024-07-09 23:20:54 +0300

    sample cmake code

---
+++ downloads-sorter/downloads-sorter.cpp
@@ -0,0 +1,12 @@
+﻿// downloads-sorter.cpp : Defines the entry point for the application.
+//
+
+#include "downloads-sorter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
